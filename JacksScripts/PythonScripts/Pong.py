2bf055d6030cdf5fb7d4ebe47b404e62f8b25933
rascalf78 <jdfoster2002@gmail.com>
2021-11-10 21:19:01 -0600

    Laptop test

--- JacksScripts/PythonScripts/Pong.py
+++ JacksScripts/PythonScripts/Pong.py
@@ -133,3 +133,6 @@ while True:
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
+
+
+#This is a test of git
