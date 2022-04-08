15ef869c66fe15f7024bb4fe10ffdce8f28e138f
rascalf78 <jdfoster2002@gmail.com>
2022-04-07 20:27:14 -0500

    idk

---
+++ JacksScripts/PythonScripts/practice.py
@@ -0,0 +1,11 @@
+import math
+
+x = [[1,2,3,4,5,6,7,8,9,10,11,12,13,14], [1,4,5]]
+
+
+for i in range(len(x)):
+     for k in x[i]:
+          print (math.sqrt(k))
+          
+
+
--- JacksScripts/PythonScripts/tempCodeRunnerFile.py
+++ JacksScripts/PythonScripts/tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
-
+for i in range(len(x)):
+     
+#      if len(x) < 2:
+#           for i in range(len(x)):
+#                print(i)
+#      else:
+#           for k in x[i]:
+#               print (math.sqrt(k))
